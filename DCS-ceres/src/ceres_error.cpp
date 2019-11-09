ad0ffc51d9f27182fbd32927faf2bc5a93d40a48
Giseop Kim <kkissbee@kaist.ac.kr>
2019-11-09 21:20:02 +0900

    gs

--- DCS-ceres/src/ceres_error.cpp
+++ DCS-ceres/src/ceres_error.cpp
@@ -3,7 +3,6 @@
 OdometryResidue::OdometryResidue
 (double dx, double dy, double dtheta)
 {
-
     this->dx = dx;
     this->dy = dy;
     this->dtheta = dtheta;
@@ -18,19 +17,26 @@ OdometryResidue::OdometryResidue
       a_Tcap_b(1,1) = cos_t;
       a_Tcap_b(0,2) = this->dx;
       a_Tcap_b(1,2) = this->dy;
-
       a_Tcap_b(2,0) = 0.0;
       a_Tcap_b(2,1) = 0.0;
       a_Tcap_b(2,2) = 1.0;
   }
-
 } // constructor, OdometryResidue
 
+
 static ceres::CostFunction* 
-OdometryResidue::Create(const double dx, const double dy, const double dtheta)
+OdometryResidue::Create
+(const double dx, const double dy, const double dtheta)
 {
-    return (new ceres::AutoDiffCostFunction<OdometryResidue, 3, 3, 3>(new OdometryResidue(dx, dy, dtheta)));
+    return 
+    ( 
+      new ceres::AutoDiffCostFunction<OdometryResidue, 3, 3, 3>
+      (
+        new OdometryResidue(dx, dy, dtheta)
+      )
+    );
 } // Create, OdometryResidue
+
 
 template <typename T>
 bool 
@@ -86,3 +92,104 @@ OdometryResidue::operator()
     return true;
 } // (), OdometryResidue
 
+
+DCSClosureResidue::DCSClosureResidue 
+(double dx, double dy, double dtheta )
+{
+    this->dx = dx;
+    this->dy = dy;
+    this->dtheta = dtheta;
+
+    // make a_Tcap_b
+    {
+      double cos_t = cos( this->dtheta );
+      double sin_t = sin( this->dtheta );
+      a_Tcap_b(0,0) = cos_t;
+      a_Tcap_b(0,1) = -sin_t;
+      a_Tcap_b(1,0) = sin_t;
+      a_Tcap_b(1,1) = cos_t;
+      a_Tcap_b(0,2) = this->dx;
+      a_Tcap_b(1,2) = this->dy;
+      a_Tcap_b(2,0) = 0.0;
+      a_Tcap_b(2,1) = 0.0;
+      a_Tcap_b(2,2) = 1.0;
+  }
+}  // constructor, DCSClosureResidue
+
+
+static ceres::CostFunction* 
+DCSClosureResidue::Create
+(const double dx, const double dy, const double dtheta)
+{
+  return 
+  (
+    new ceres::AutoDiffCostFunction<DCSClosureResidue, 3, 3, 3>
+    (
+      new DCSClosureResidue(dx, dy, dtheta)
+    )
+  );
+} // Create, DCSClosureResidue
+
+
+template <typename T> 
+bool 
+DCSClosureResidue::operator() 
+(const T* const P1, const T* const P2, T* e) const
+{
+    // Convert P1 to T1 ^w_T_a
+    Eigen::Matrix<T,3,3> w_T_a;
+    {
+      T cos_t = T(cos( P1[2] ));
+      T sin_t = T(sin( P1[2] ));
+      w_T_a(0,0) = cos_t;
+      w_T_a(0,1) = -sin_t;
+      w_T_a(1,0) = sin_t;
+      w_T_a(1,1) = cos_t;
+      w_T_a(0,2) = P1[0];
+      w_T_a(1,2) = P1[1];
+      w_T_a(2,0) = T(0.0);
+      w_T_a(2,1) = T(0.0);
+      w_T_a(2,2) = T(1.0);
+    }
+
+    // Convert P2 to T2 ^w_T_a
+    Eigen::Matrix<T,3,3> w_T_b;
+    {
+        T cos_t = cos( P2[2] );
+        T sin_t = sin( P2[2] );
+        w_T_b(0,0) = cos_t;
+        w_T_b(0,1) = -sin_t;
+        w_T_b(1,0) = sin_t;
+        w_T_b(1,1) = cos_t;
+        w_T_b(0,2) = P2[0];
+        w_T_b(1,2) = P2[1];
+        w_T_b(2,0) = T(0.0);
+        w_T_b(2,1) = T(0.0);
+        w_T_b(2,2) = T(1.0);
+    }
+
+    // cast from double to T
+    Eigen::Matrix<T, 3, 3> T_a_Tcap_b;
+    T_a_Tcap_b <<   T(a_Tcap_b(0,0)), T(a_Tcap_b(0,1)),T(a_Tcap_b(0,2)),
+                    T(a_Tcap_b(1,0)), T(a_Tcap_b(1,1)),T(a_Tcap_b(1,2)),
+                    T(a_Tcap_b(2,0)), T(a_Tcap_b(2,1)),T(a_Tcap_b(2,2));
+
+    // now we have :: w_T_a, w_T_b and a_Tcap_b
+    // compute pose difference
+    Eigen::Matrix<T,3,3> diff = T_a_Tcap_b.inverse() * (w_T_a.inverse() * w_T_b);
+
+    // ... as far as here, same as the original residual
+
+    // refer: http://www2.informatik.uni-freiburg.de/~spinello/agarwalICRA13.pdf
+    float const_upper_bound = 0.5; // USER PARAMETER, notation phi at the original paper 
+    T res = diff(0,2)*diff(0,2) + diff(1,2)*diff(1,2); // original error term for each loop closing constraint
+    T psi_org = sqrt( T(2.0) * T(const_upper_bound) / ( T(const_upper_bound) + res ) );
+    T psi = std::min( T(1.0), psi_org ) ;
+
+    // if psi is constant (i.e., 1), this goes the original (not robust one) 
+    e[0] = psi * diff(0,2);
+    e[1] = psi * diff(1,2);
+    e[2] = psi * asin( diff(1,0) );
+
+    return true;
+} // (), DCSClosureResidue
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 - Goal: Toy DCS for understand the importance of a robust back-end system for robust SLAM.
 
 ### Notes
-- The many parts of this repository's code are barrowed from https://github.com/mpkuse/toy-pose-graph-optimization-ceres
-- I added some useful other datasets from [Luca Carlone's site](https://lucacarlone.mit.edu/datasets/) 
-- I refactored codes and directory structures for better understanding 
-- (To do) I added useful results discussion with repect to DCS On/Off, a portion of outliers, and datasets. 
-- (To do) I added code-block-explanation slides.
+- The many parts of this repository's code are barrowed from https://github.com/mpkuse/toy-pose-graph-optimization-ceres.
+- I added some useful other datasets from [Luca Carlone's site](https://lucacarlone.mit.edu/datasets/). 
+- I refactored codes and directory structures for better understanding.
+- I added my own results and discussions with repect to DCS On/Off, a portion of outliers, and datasets. 
+- I added code-block-explanation slides.
 - (To do) I extended the original 2D example to 3D (i.e., SE3) examples. 
 
 ### How to run 
@@ -21,12 +21,26 @@ $ ./do_build.sh INTEL 50 1 # USING DCS, with 50 outlier loops on INTEL dataset
 $ ./do_build.sh INTEL 50 0 # NOT USING DCS, with 50 outlier loops on INTEL dataset
 ```
 
+### What is DCS?
+- As its name, literally, it dynamically scales the covariance. 
+- At the code level, it is more easy to understand (just 4 lines are added.)
+- If $\psi$ (denoted $s$ in the original paper) is equal to $1$, it is same as the original one, which is not robust to the outlier loops.
+
+![code_ex](./DCS-ceres/docs/code_Ex.png)
+
+- Here, the detailed exaplanation of the mathematical derivation is ommitted and refer the [original paper](http://www2.informatik.uni-freiburg.de/~spinello/agarwalICRA13.pdf) 
+- Simply, $\Phi$ is a free parameter (i.e., user parameter). In the author's [original paper [ICRA 13)](http://www2.informatik.uni-freiburg.de/~spinello/agarwalICRA13.pdf), they changed this value from 0.1 to 10 and reported the performances for those ranges.
+
+
 ### Experimental results 
 #### dataset: INTEL
 
+![intel_result](./DCS-ceres/docs/report.png)
+- These examples were acquired under default ceres solver option and $\Phi = 0.5$.
+- As expected, without the robust back-end system such as DCS, the optimized graph topology easily collapsed.
+- More exmaples are found in DCS-ceres/docs/{DATASET_NAME}.
 
 
-#### dataset: CSAIL
 
 
 
